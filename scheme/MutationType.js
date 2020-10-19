51f0e856c9fad8a44f9889b33da976a7993576a8
IdaliaGpe <idaliaguadalupepucca@hotmail.com>
2020-10-19 10:25:26 -0700

    sale

--- scheme/MutationType.js
+++ scheme/MutationType.js
@@ -1,4 +1,4 @@
-import graphql from 'graphql';
+import graphql, { GraphQLList } from 'graphql';
 import Product from '../models/Product.js';
 import ProductGroup from '../models/ProductGroup.js';
 
@@ -8,6 +8,10 @@ import UserType from './UserType.js';
 import AddressType from './AddressType.js';
 import User from '../models/User.js';
 import Address from '../models/Address.js';
+import OrderType from './OrderType.js';
+import Order from '../models/Order.js';
+import SaleType from './SaleType.js';
+import Sale from '../models/Sale.js';
 
 const {GraphQLID, GraphQLString, GraphQLFloat,GraphQLObjectType} = graphql;
 
@@ -105,6 +109,28 @@ const MutationType = new GraphQLObjectType({
             resolve(parent, args){
                 const address = new Address(args);
                 return address.save();
+            }
+        },
+        //Orders
+        addOrder: {
+            type: OrderType,
+            args: {userId: {GraphQLID}},
+            resolve(parent, args){
+                const order = new Order(args);
+                return order.save();
+            }
+        },
+        //Sales
+        addSales: {
+            type: SaleType,
+            args: {
+                productId: {type: GraphQLID},
+                orderId: {type: GraphQLID},
+                timeStamp: {type: GraphQLString}
+            },
+            resolve(parent, args){
+                const sale = new Sale(args);
+                return sale.save();
             }
         }
     }
